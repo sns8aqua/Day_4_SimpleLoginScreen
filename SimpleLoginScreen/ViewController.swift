2358edaeae401c4c881613a4833fdfc27beba3d1
Santhosh <clicomdeveloper@gmail.com>
2022-01-09 22:25:42 +0530

    Initial Commit

---
+++ SimpleLoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLoginScreen
+//
+//  Created by Santhosh on 09/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
